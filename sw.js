df11a9c3c8d628dacb3cb9089ad288c10b5dd329
Claudio Venegas <cvenegasvv@gmail.com>
2026-07-02 18:23:36 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'cva-v1';
+const ASSETS = ['./index.html', './manifest.json', 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached)));
+});
